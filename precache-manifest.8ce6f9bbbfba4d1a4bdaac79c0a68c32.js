e03734d7953934a826bcf807a5f0e5a7c9afb661
mwaqarnadeem <waqar.usuf@gmail.com>
2019-12-20 03:50:00 +0500

    Updates

---
+++ precache-manifest.8ce6f9bbbfba4d1a4bdaac79c0a68c32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebba03dd757e2059765031761b0e0c71",
+    "url": "/iotfirebase/index.html"
+  },
+  {
+    "revision": "e24223d0ac58b398dec3",
+    "url": "/iotfirebase/static/css/main.6c25cdd9.chunk.css"
+  },
+  {
+    "revision": "960d9f59b6af5dd753b8",
+    "url": "/iotfirebase/static/js/2.528cc7fe.chunk.js"
+  },
+  {
+    "revision": "b03343d1dfdf0242e0f4ba3806a19844",
+    "url": "/iotfirebase/static/js/2.528cc7fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e24223d0ac58b398dec3",
+    "url": "/iotfirebase/static/js/main.471682a8.chunk.js"
+  },
+  {
+    "revision": "8bb7802b316857070bc3",
+    "url": "/iotfirebase/static/js/runtime-main.198520c7.js"
+  }
+]);
